a1b26d5995fbc7767821d4db462a3691df425ce9
Johnny Gu <johnny@wiredcraft.com>
2017-04-27 18:02:35 +0800

    tmp

---
+++ CFA/ScheduleDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ScheduleDetailViewController.swift
+//  CFA
+//
+//  Created by Johnny Gu on 27/04/2017.
+//  Copyright © 2017 Johnny Gu. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleDetailViewController: UIViewController {
+
+    var schedule: Schedule!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
---
+++ CFA/VideoDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  VideoDetailViewController.swift
+//  CFA
+//
+//  Created by Johnny Gu on 27/04/2017.
+//  Copyright © 2017 Johnny Gu. All rights reserved.
+//
+
+import UIKit
+
+class VideoDetailViewController: UIViewController {
+
+    @IBOutlet weak var playBtn
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
